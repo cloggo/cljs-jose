640dfe86b56c38434d912a021ffa01a045e70efb
Aaron Diep <aaron@delite.ca>
2018-09-18 20:25:16 -0600

    deploy to clojars

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.cloggo/jose "0.1.0"
+  :description "clojurescript API to access javascript c-jose"
+  :url "https://github.com/cloggo/cljs-jose"
+  :license {:name "MIT" }
+  :dependencies [[binaryage/oops "0.6.2"]
+                 [org.clojure/clojure "1.8.0"]])
